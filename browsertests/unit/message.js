1ba081bbdb3c3d654b5bce730ee9d3af1a2643c6
Tobias Södergren <tobias.sodergren@infomaker.se>
2014-11-21 13:13:24 +0100

    Additional unicode characters decoded correctly in test

--- browsertests/unit/message.js
+++ browsertests/unit/message.js
@@ -22,7 +22,7 @@ test("Send and receive a message", function() {
 test("Send and receive a message with a JSON body", function() {
   
   var client = Stomp.client(TEST.url);
-  var payload = {text: "h\u00E4llo", bool: true, value: Math.random()};
+  var payload = {text: "hello", bool: true, value: Math.random()};
   
   client.connect(TEST.login, TEST.password,
     function() {
@@ -38,5 +38,27 @@ test("Send and receive a message with a JSON body", function() {
       
       client.send(TEST.destination, {}, JSON.stringify(payload));
     });
+    stop(TEST.timeout);
+});
+
+test("Send and receive a message with a UTF-8 JSON body", function() {
+
+    var client = Stomp.client(TEST.url);
+    var payload = {text: "\u0050 \u00E4 \u0496 \u09a8 \u1C4F \uD7A3 \uD800\uDD28", bool: true, value: Math.random()};
+
+    client.connect(TEST.login, TEST.password,
+        function() {
+            client.subscribe(TEST.destination, function(message)
+            {
+                start();
+                var res = JSON.parse(message.body);
+                equals(res.text, payload.text);
+                equals(res.bool, payload.bool);
+                equals(res.value, payload.value);
+                client.disconnect();
+            });
+
+            client.send(TEST.destination, {}, JSON.stringify(payload));
+        });
     stop(TEST.timeout);
 });
